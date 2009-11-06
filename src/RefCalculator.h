663fdd66e02aaec7ed184f9f99b1296fabeeb6bd
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2009-11-06 07:54:34 +0000

    changed pthread join code

--- src/RefCalculator.h
+++ src/RefCalculator.h
@@ -12,16 +12,24 @@
 
 
 
-int AbelesCalcAll(double *coefP, double *yP, double *xP,long npoints, int Vmullayers, int Vmulappend, int Vmulrep);
+int AbelesCalcAll(const double *coefP, double *yP, const double *xP,long npoints, int Vmullayers, int Vmulappend, int Vmulrep);
 int AbelesCalc_ImagAll(double *coefP, double *yP, double *xP,long npoints, int Vmullayers, int Vmulappend, int Vmulrep);
 
+//a structure that described the variables required to calculate the reflectivity
 typedef struct{
+	//number of Q points we have to calculate
 	long npoints;
+	//how many layers in the multilayer (optional, but if not specified should be 0)
 	int Vmullayers;
+	//where the multilayer is appended to the basic model.
 	int Vappendlayer;
+	//how many repeats in the multilayer
 	int Vmulrep;
+	//a double array containing the model coefficients (assumed to be correct)
 	double *coefP;
+	//the Reflectivity values to return
 	double *yP;
+	//the Q values to do the calculation for.
 	double *xP;	
 }  refCalcParm;
 
