f7d437d79f1d1a608b0574c1f80b7fb6d07fe2b8
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2009-11-11 05:55:54 +0000

    finished experimenting with threadpooling

--- src/RefCalculator.cpp
+++ src/RefCalculator.cpp
@@ -28,6 +28,9 @@ void *AbelesThreadWorker(void *arg){
 	refCalcParm *p = (refCalcParm *) arg;
 	err = AbelesCalcAll(p->coefP, p->yP, p->xP, p->npoints, p->Vmullayers, p->Vappendlayer, p->Vmulrep);
 	
+#ifdef _WINDOWS_
+	pthread_win32_thread_detach_np();
+#endif
 	pthread_exit((void*)err);
 	return NULL;
 }
