90261c958bc43d18124d1f760828fa80c4d1fd86
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2013-01-30 02:12:42 +0000

    made version of XOP that can handle arbitrary integration precision for resolution smearing convolution

---
+++ packages/abeles/trunk/src/GaussWeights.cpp
@@ -0,0 +1,58 @@
+//
+//  GaussWeights.cpp
+//  
+//
+//  Created by Andrew Nelson on 30/01/13.
+//
+//
+
+#include "GaussWeights.h"
+#include "gauss_legendre.h"
+
+GaussWeight::GaussWeight(){};
+
+GaussWeight::~GaussWeight(){};
+
+GaussWeights::GaussWeights(){};
+
+GaussWeights::~GaussWeights(){};
+
+void GaussWeights::getGaussWeight(int n, std::vector<double> &x, std::vector<double> &w){
+    std::map<int, GaussWeight>::iterator it;
+    GaussWeight gw;
+    int i, m;
+
+    it = gaussWeightsStorage.find(n);
+    
+    if(it == gaussWeightsStorage.end()){
+        //get new gauss weights
+        m = (n + 1) >> 1;
+        gw.w.resize(m);
+        gw.x.resize(m);
+       
+        gauss_legendre_tbl(n, &gw.x[0], &gw.w[0], 1.0e-17);
+        
+        std::reverse(gw.x.begin(),gw.x.end());
+        std::reverse(gw.w.begin(),gw.w.end());
+        
+        for(i = m - 1 - (n & 1); i >= 0; i--){
+            gw.x.push_back(-1. * gw.x[i]);
+            gw.w.push_back(gw.w[i]);
+        }
+        
+        std::reverse(gw.x.begin(),gw.x.end());
+        
+        gaussWeightsStorage[n] = gw;
+        x.resize(gw.x.size());
+        w.resize(gw.x.size());
+        
+        x.assign(gw.x.begin(), gw.x.end());
+        w.assign(gw.w.begin(), gw.w.end());
+    } else {
+        x.resize(it->second.x.size());
+        w.resize(it->second.w.size());
+        
+        x.assign(it->second.x.begin(), it->second.x.end());
+        w.assign(it->second.w.begin(), it->second.w.end());
+    }
+};
